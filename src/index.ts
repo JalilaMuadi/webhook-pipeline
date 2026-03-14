44a3d3fb665411ec1a5a090530e025f6dbe2bf87
jalilaMuadi <jalilahnm@gmail.com>
2026-03-14 17:19:29 +0200

    Add pipelines API endpoints (GET & POST) with basic testing

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,57 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+// src/index.ts
+import "dotenv/config";
+import { eq } from "drizzle-orm";
+import { db } from "./db/index.js"; 
 import { pipelines } from "./db/schema.js";
 
-const pool = new Pool({
-  connectionString: process.env.DB_URL || "postgres://postgres:postgres@localhost:5432/chirpy?sslmode=disable",
+import express, {Request, Response, NextFunction} from "express";
+
+import { createPipeline, getPipelines } from "./db/queries/pipelines.js";
+
+// ============================= 
+// Express app setup
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+// =============================
+//get all pipelines 
+app.get("/api/pipelines", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const pipelines = await getPipelines();
+    res.status(200).json(pipelines);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-const db = drizzle(pool);
+// =============================
+// create a new pipeline
+app.post("/api/pipelines", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name, processing_type } = req.body;
+    if (!name || !processing_type) {
+      return res.status(400).json({ error: "Missing name or processing_type" });
+    }
 
-async function testDB() {
-  const all = await db.select().from(pipelines);
-  console.log("pipelines:", all);
-}
+    const newPipeline = await createPipeline(name, processing_type);
+    res.status(201).json(newPipeline);
+  } catch (err) {
+    next(err);
+  }
+});
 
-testDB();
+// =============================
+// Error middleware
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+
+// =============================
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
