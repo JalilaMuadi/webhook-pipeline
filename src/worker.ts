0e0fbd0c79f5a9cf818d2e4318c4b34842f6f879
jalilaMuadi <jalilahnm@gmail.com>
2026-03-18 21:21:20 +0200

    implement uppercase transformation logic in worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,36 +1,32 @@
-import { getPendingJobs, updateJobStatus } from "./db/queries/jobs.js";
+import { getPendingJobsWithDetails, updateJobStatus } from "./db/queries/jobs.js";
 
 async function processJobs() {
-  // Fetch all pending jobs that are waiting to be processed
-  const pendingJobs = await getPendingJobs();
-
-  if (pendingJobs.length > 0) {
-    console.log(`[Worker] Found ${pendingJobs.length} jobs to process.`);
-  }
+  const pendingJobs = await getPendingJobsWithDetails();
 
   for (const job of pendingJobs) {
     try {
-      console.log(`[Worker] Processing job ${job.id}...`);
-      
-      // Update status to 'processing' so no other worker instance picks it up
       await updateJobStatus(job.id, "processing");
 
-      // TODO: Implement processing logic (e.g., Uppercase conversion) in the next phase
-      // For now, we are just logging the payload as a simulation
-      console.log(`[Worker] Payload is: ${job.payload}`);
+      let finalPayload = job.payload;
 
-      // Mark the job as completed successfully
+      switch (job.processingType) {
+        case "uppercase":
+          finalPayload = job.payload.toUpperCase();
+          break;
+        default:
+          console.log(`[Worker] Unknown type: ${job.processingType}, skipping...`);
+      }
+
+      console.log(`[Worker] Job ${job.id} processed as ${job.processingType}.`);
+      console.log(`[Worker] Result: ${finalPayload}`);
+
       await updateJobStatus(job.id, "completed");
-      console.log(`[Worker] Job ${job.id} done!`);
 
     } catch (err) {
-      console.error(`[Worker] Error processing job ${job.id}:`, err);
-      // Update status to 'failed' if an error occurs during processing
+      console.error(`[Worker] Error:`, err);
       await updateJobStatus(job.id, "failed");
     }
   }
 }
 
-// Polling: Run the worker every 10 seconds
-console.log("Worker is running and waiting for jobs...");
 setInterval(processJobs, 10000);
